035974c0ea5822e14fd541b20db6afbdc2d31657
YifeiGong <yifeiaimeegong@hotmail.com>
2019-10-20 19:43:50 -0500

    new UI

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ import './App.css';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import Card from '@material-ui/core/Card';
-<<<<<<< Updated upstream
-=======
 import Chip from '@material-ui/core/Chip';
 import Box from '@material-ui/core/Box';
 import { Container } from '@material-ui/core';
 import { positions } from '@material-ui/system';
->>>>>>> Stashed changes
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+
 
 import firebase from 'firebase/app';
 import 'firebase/database';
@@ -37,35 +40,38 @@ firebase.initializeApp(firebaseConfig);
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
+    flexWrap: 'wrap',
   },
+  formControl: {
+    margin: theme.spacing(2),
+    minWidth: 300,
+  },
+
   title: {
     flexGrow: 1,
   },
     gridList: {
     width: "100%",
-<<<<<<< Updated upstream
-    height: 600,
-=======
     
    
->>>>>>> Stashed changes
   },
   card:{
     margin: 20,
   },
   paper: {
-    background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+   // background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
     position: "fixed",
     padding: theme.spacing(2),
     textAlign: 'left',
     color: theme.palette.text.secondary,
     whiteSpace: 'nowrap',
+    marginTop:'200px',
     margin: theme.spacing(2),
   },
 }));
 
-const AppBar_header = () => {
+const AppBar_header =  ({filterOnOff, setFilterOnOff, selectedTime, setSelectedTime, budget, setBudget, numPeople, setNumPeople, restaurants, setRestaurantsAll, filteredRestaurants}) => {
   const classes = useStyles()
 
   return (
@@ -79,104 +85,189 @@ const AppBar_header = () => {
           <Button color="inherit">Login</Button>
         </Toolbar>
         </Container>
+        <Container>
+        <Toolbar>
+        <Grid container>
+<Grid item xs={2}>
+        <TeamMemberFilter numPeople={numPeople} setNumPeople={setNumPeople}></TeamMemberFilter>
+         </Grid>
+         <Grid item xs={2}>
+           <BudgetFilter budget={budget} setBudget={setBudget}></BudgetFilter>
+           </Grid> 
+           <Grid item xs={4}>
+             <DateFilter filterOnOff={filterOnOff} setFilterOnOff={setFilterOnOff} setRestaurantsAll={setRestaurantsAll} filteredRestaurants={filteredRestaurants}
+          selectedTime={selectedTime} setSelectedTime={setSelectedTime}></DateFilter>
+          </Grid> 
+           <Grid item xs={4}>
+             <TimeFilter filterOnOff={filterOnOff} setFilterOnOff={setFilterOnOff} setRestaurantsAll={setRestaurantsAll} filteredRestaurants={filteredRestaurants}
+          selectedTime={selectedTime} setSelectedTime={setSelectedTime}></TimeFilter>
+          </Grid> 
+          
+         </Grid>
+         </Toolbar>
+        </Container>
       </AppBar>
     </div>
   )
 }
 
-
-const TeamMemberFilter = () => {
+const TeamMemberFilter = ({numPeople, setNumPeople}) => {
   const classes = makeStyles(theme => ({
     TextField: {
       width: '20px',
     }
   }));
+  // function handle
 
   return(
     <div>
-      <Typography variant="h5" component="h3">Our team has</Typography>
+      
       <form class="filter">
-<<<<<<< Updated upstream
-        <TextField id="party-size" className={classes.textField} margin="normal"></TextField>
-=======
       <TextField
         id="standard-with-placeholder"
-        label="Number of members"
+        label="Our team has"
         placeholder="Number of members"
         className={classes.textField}
         margin="normal"
         onChange={(e) => setNumPeople(e.target.value)} id="party-size" className={classes.textField} margin="normal"></TextField>
->>>>>>> Stashed changes
       </form>
       </div>
   )
 }
 
-const BudgetFilter = () => {
+const BudgetFilter = ({budget, setBudget}) => {
+  const classes = makeStyles(theme => ({
+    TextField: {
+      width: '20px',
+    }
+  }));
   return(
     <div>
-      <Typography variant="h5" component="h3">Our team building budget is</Typography>
+      
       <form class="filter">
-        <TextField id="party-budget" margin="normal"></TextField>
+        <TextField id="standard-with-placeholder"
+        label="We have"
+        placeholder="$ budget"
+        className={classes.textField}
+        margin="normal"
+        onChange={(e) => setBudget(e.target.value)} id="party-budget" margin="normal"></TextField>
       </form>
-<<<<<<< Updated upstream
-        
-      <Button variant="contained">
-        Average
-      </Button>
-    </Paper>
-=======
         </div> 
->>>>>>> Stashed changes
   )
 }
 
-const TimeFilter = () => {
+const TimeFilter = ({selectedTime, setSelectedTime, filterOnOff, setFilterOnOff, setRestaurantsAll, filteredRestaurants}) => {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    age: '',
+  });
+
+  const inputLabel = React.useRef(null);
+
+
+  const handleChange = event => {
+    setValues(oldValues => ({
+      ...oldValues,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  return (
+    <form className={classes.root} autoComplete="off">
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="age-simple">for</InputLabel>
+        <Select
+          value={values.age}
+          onChange={handleChange}
+          inputProps={{
+            name: 'age',
+            id: 'age-simple',
+          }}
+        >
+          <MenuItem value={10}>Lunch 11:30AM-1:30PM</MenuItem>
+          <MenuItem value={20}>Dinner 5:30PM-7:30PM</MenuItem>
+          
+        </Select>
+      </FormControl>
+      </form>
+      )
+}
+
+const DateFilter = ({selectedTime, setSelectedTime, filterOnOff, setFilterOnOff, setRestaurantsAll, filteredRestaurants}) => {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    age: '',
+  });
+
+  const inputLabel = React.useRef(null);
+
+
+  const handleChange = event => {
+    setValues(oldValues => ({
+      ...oldValues,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  return (
+    <form className={classes.root} autoComplete="off">
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="age-simple">We would like to go on</InputLabel>
+        <Select
+          value={values.age}
+          onChange={handleChange}
+          inputProps={{
+            name: 'age',
+            id: 'age-simple',
+          }}
+        >
+          <MenuItem value={10}>Next 5 workdays</MenuItem>
+          <MenuItem value={20}>Next 7 workdays</MenuItem>
+          
+        </Select>
+      </FormControl>
+      </form>
+      )
+}
+
+const TimeFilterr = ({selectedTime, setSelectedTime, filterOnOff, setFilterOnOff, setRestaurantsAll, filteredRestaurants}) => {
+  const handleOnClick = (time) => {
+    console.log("filtering")
+    setSelectedTime(time);
+    setFilterOnOff(!filterOnOff);
+    setRestaurantsAll(filteredRestaurants);
+  }
+
   return(
     <div>
-      <Typography variant="h5" component="h3">We'd like to go during:</Typography>
+      <Typography variant="h5" component="h5">We'd like to go during:</Typography>
 
-      <Button variant="contained">
+      <Button onClick={(e) => handleOnClick("11:30-1:30")} variant="contained">
         Lunch 11:30AM-1:30PM
       </Button>
-<<<<<<< Updated upstream
-
-      <Button variant="contained">
-=======
-<br></br><br></br>
       <Button onClick={(e) => handleOnClick("5:30-7:30")} variant="contained">
->>>>>>> Stashed changes
         Dinner 5:30PM-7:30PM
       </Button>
     </div>
   )
 }
 
-const Pool = () => {
+const Poll = ({pollRests}) => {
   const classes = useStyles()
-<<<<<<< Updated upstream
-
-  return(
-    <div>
-      <Typography variant="h5" component="h3">Pool</Typography>
-<Card className={classes.card}>
-=======
  
   
   return(
     <div>
       <Typography variant="h5" component="h3">Poll</Typography>
       <GridList>
-        <GridListTile height="450">
+        <GridListTile>
         <Card className={classes.card}>
   
->>>>>>> Stashed changes
   <Typography>Selected Restaurant 1</Typography>
-  
- 
+
 </Card>
 <Card className={classes.card}>
-  <Typography>Selected Restaurant 1</Typography>
+  <Typography>Selected Restaurant 2</Typography>
  
 </Card>
 <Card className={classes.card}>
@@ -207,48 +298,28 @@ const Pool = () => {
   )
 }
   
-<<<<<<< Updated upstream
-const GridComponent = ({restaurants}) => {
-  return (
-    <div>
-      <Grid container spacing={10}>
-      <Grid item xs={1}></Grid>
-      <Grid item xs={4}>
-          <TeamMemberFilter></TeamMemberFilter>
-          <BudgetFilter></BudgetFilter>
-          <TimeFilter></TimeFilter>
-          <Pool></Pool>
-          <Button variant="contained" color="primary">Send out pool</Button>
-      </Grid>
-      <Grid item xs={6}>
-=======
 const GridComponent = ({filterOnOff, setFilterOnOff, selectedTime, setSelectedTime, budget, setBudget, numPeople, setNumPeople, restaurants, setRestaurantsAll, filteredRestaurants}) => {
   const classes = useStyles();
   return (
     <div>
+      
         <Container>
         <Grid container spacing={3}>
 
-<Grid item xs={5}>
+        <Grid item xs={8}>
+        <RestaurantList restaurants={restaurants}/>
+      </Grid>
+
+<Grid item xs={4}>
 <Paper className={classes.paper}>
-<Typography variant="h6" className={classes.title}>
-            Teamie
-          </Typography>
-          <TeamMemberFilter numPeople={numPeople} setNumPeople={setNumPeople}></TeamMemberFilter>
-          <BudgetFilter budget={budget} setBudget={setBudget}></BudgetFilter>
-          <TimeFilter filterOnOff={filterOnOff} setFilterOnOff={setFilterOnOff} setRestaurantsAll={setRestaurantsAll} filteredRestaurants={filteredRestaurants}
-          selectedTime={selectedTime} setSelectedTime={setSelectedTime}></TimeFilter>
-          <br/>
+
+          
           <Poll></Poll>
           
           </Paper>
           
          
           </Grid>
-        <Grid item xs={7}>
->>>>>>> Stashed changes
-        <RestaurantList restaurants={restaurants}/>
-      </Grid>
     
       </Grid>
       </Container>
@@ -265,17 +336,30 @@ const RestaurantList = ({restaurants}) => {
     <GridList className={classes.gridList}>
         <GridListTile cols={2} style={{ height: 'auto' }}>
         { restaurants.map(restaurant => <Restaurant key={restaurant.id} restaurant={ restaurant } />) }
-
         </GridListTile>
-        
-           
       </GridList>
-
   )
 }
 
+
+
 const Restaurant = ({key, restaurant}) => {
   // const {name, price, tables, type} = this.props
+  
+  const [btnToggle, toggleBtn] = useState(false);
+  const [btnColor, setBtnColor] = useState("default");
+
+  const handleClick = () => {
+    const btnToggleState = !btnToggle;
+    if (btnToggle) {
+      setBtnColor("default");
+    }
+    else {
+      setBtnColor("primary");
+    }
+    toggleBtn(btnToggleState);
+  }
+
   return(
     <div className="restaurant-card">
      <Grid container spacing={3}>
@@ -290,7 +374,7 @@ const Restaurant = ({key, restaurant}) => {
   
   <Grid item xs={3}>
 <img src={imgURL}/>
-<Button variant="outlined" color="primary">Add to list</Button>
+<Chip label="Add to list" clickable color={btnColor} onClick={handleClick}/>
 </Grid>
 
 </Grid>
@@ -299,12 +383,20 @@ const Restaurant = ({key, restaurant}) => {
 )}
 
 
-
-
-const App = () => {
+const App = ({}) => {
   const [restaurantsAll, setRestaurantsAll] = useState({restaurants: []});
   const url = '/data/restaurants.json';
+  const filteredUrl = '/data/restaurantsFiltered.json';
+  const [filteredRestaurants, setFilteredRestaurants] = useState({restaurants: []})
   
+  //filter
+  const [numPeople, setNumPeople] = useState("");
+  const [budget, setBudget] = useState("");
+  const [selectedTime, setSelectedTime] = useState("");
+
+  //garbage
+  const [filterOnOff, setFilterOnOff] = useState(false);
+
   useEffect(() => {
     const fetchRestaurants = async () => {
       const response = await fetch(url);
@@ -312,21 +404,23 @@ const App = () => {
       const json = await response.json();
       setRestaurantsAll(json);
     }
+    const fetchFilteredRestaurants = async () => {
+      const response = await fetch(filteredUrl);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setFilteredRestaurants(json);
+    }
     fetchRestaurants();
+    fetchFilteredRestaurants();
   }, [])
   
+
   const classes = useStyles();
 
   return (
-<<<<<<< Updated upstream
-    <div>
-      <AppBar_header/>
-      <GridComponent restaurants={restaurantsAll.restaurants} />
-      
-=======
     <div class="container">
       
-      
+      <AppBar_header></AppBar_header>
       <GridComponent 
       filterOnOff={filterOnOff}
       setFilterOnOff={setFilterOnOff}
@@ -343,7 +437,6 @@ const App = () => {
       />
           
   
->>>>>>> Stashed changes
     </div>
   );
 }
